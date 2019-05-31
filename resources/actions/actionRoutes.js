9f0b43e8755f6b2d96804823948e7ec543b1cb0b
greenhornsr <srollins11279@gmail.com>
2019-05-31 09:05:53 -0700

    modified get by id to return a more appropriate response if the project is null

--- resources/actions/actionRoutes.js
+++ resources/actions/actionRoutes.js
@@ -21,11 +21,16 @@ router.get('/:id', (req, res) => {
     const { id } = req.params
     db.get(id)
     .then(action => {
-        res.status(200).json({ success: true, message: "Got one!", action })
+        if (action){
+            res.status(200).json({ success: true, message: "Got one!", action })
+        }else{
+            console.log(err)
+            res.status(400).json({ success: false, message: "ewww, no such luck.  No Action Found!", err })
+        }
     })
     .catch(err => {
         console.log(err)
-        res.status(400).json({ success: false, message: "ewww, no such luck.  No Action Found!", err })
+        res.status(500).json({ success: false, message: "we got nothing for ya!", err })
     })
 })
 
--- resources/projects/projectRoutes.js
+++ resources/projects/projectRoutes.js
@@ -21,11 +21,16 @@ router.get('/:id', (req, res) => {
     const { id } = req.params
     db.get(id)
     .then(project => {
-        res.status(200).json({ success: true, message: "Got one!", project })
+        if (project){
+            res.status(200).json({ success: true, message: "Got one!", project })
+        }else{
+            console.log(err)
+            res.status(400).json({ success: false, message: "ewww, no such luck.  No Action Found!", err })
+        }
     })
     .catch(err => {
         console.log(err)
-        res.status(400).json({ success: false, message: "ewww, no such luck.  No Action Found!", err })
+        res.status(500).json({ success: false, message: "we got dem server issues!", err })
     })
 })
 
