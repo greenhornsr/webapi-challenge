7aa0114b4ddc436705e3a68a50ab52ad515ce66b
greenhornsr <srollins11279@gmail.com>
2019-05-31 09:40:39 -0700

    added post logic for actions and middleware to check for required object fields.  confirmed working

--- resources/actions/actionRoutes.js
+++ resources/actions/actionRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const db = require('../../data/helpers/actionModel');
+const pdb = require('../../data/helpers/projectModel');
+
 
 const router = express.Router();
 router.use(express.json());
@@ -34,7 +36,16 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateAction, validateProject_Id, (req, res) => {
+    const newaction = req.body;
+    db.insert(newaction)
+    .then(action => {
+        res.status(201).json({ success: true, message: `${newaction.description} was added successfully!`, action })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ success: false, message: 'server error', err})
+    })
 })
 
 router.put('/', (req, res) => {
@@ -45,7 +56,36 @@ router.delete('/', (req, res) => {
 
 })
 
+// Middleware
+function validateAction(req, res, next) {
+    // console.log('im in validateUser')
+    if(!req.body.description){
+        res.status(400).json({ message: 'Missing required description field' })
+    }else if(!req.body.notes){
+        res.status(400).json({ message: 'Missing required notes field' })
+    }else if(!req.body.project_id){
+        res.status(400).json({ message: 'Missing required project_id field' })
+    }else{
+        next();
+    }
+};
 
+
+function validateProject_Id(req, res, next) {
+    // console.log(req.params)
+    pdb.get(req.params.project_id)
+    .then(p_id => {
+        if(p_id){ 
+        next()
+        }else{
+            res.status(400).json({ message: 'Invalid project_id!' })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'unknown server error validating id', err })
+    })
+} 
 
 
 module.exports = router;
